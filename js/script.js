dbfcfc179742540dd3808eda9301042e28abc214
Nikola Stefanovski <nikoding32@gmail.com>
2022-10-31 18:07:14 +0100

    Small adjustments (Task3).

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ let pokemonList = [
     {name: 'Hitmonlee', height: '150'}
 ];
 
+// Writes the list of pokemons to the DOM and singles out the large pokemon with a comment next to their height.
 for (let i = 0; i < pokemonList.length; i++) {
   if(pokemonList[i].height > 100) {
-    document.write('<p class="jscript">' + pokemonList[i].name + '  ' + 'height: \(' + pokemonList[i].height + ' cm' + '\).  ' + 'Wow, this one is huge!' + '</p>' + '<br>');
+    document.write('<p class="jscript">' + pokemonList[i].name + '\'s  ' + 'height: ' + pokemonList[i].height + ' cm. ' + 'Wow, this one is huge!' + '</p>' + '<br>');
   } else {
-    document.write('<p class="jscript">' + pokemonList[i].name + '  ' + 'height: \(' + pokemonList[i].height + ' cm' + '\)  ' + '</p>' + '<br>');
+    document.write('<p class="jscript">' + pokemonList[i].name + '\'s  ' + 'height: ' + pokemonList[i].height + ' cm.' + '</p>' + '<br>');
     }
   }
