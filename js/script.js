e0c57842646ffd8435775ef49b1fdb006d2a486b
Nikola Stefanovski <nikoding32@gmail.com>
2022-10-31 17:57:47 +0100

    Task 1.3. (Added style and a for loop

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 let pokemonList = [
-    {name: 'Bulbasaur', heigth: '70'},
-    {name: 'Charmander', heigth: '60'},
-    {name: 'Hitmonlee', heigth: '150'}
+    {name: 'Bulbasaur', height: '70'},
+    {name: 'Charmander', height: '60'},
+    {name: 'Hitmonlee', height: '150'}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if(pokemonList[i].height > 100) {
+    document.write('<p class="jscript">' + pokemonList[i].name + '  ' + 'height: \(' + pokemonList[i].height + ' cm' + '\).  ' + 'Wow, this one is huge!' + '</p>' + '<br>');
+  } else {
+    document.write('<p class="jscript">' + pokemonList[i].name + '  ' + 'height: \(' + pokemonList[i].height + ' cm' + '\)  ' + '</p>' + '<br>');
+    }
+  }
